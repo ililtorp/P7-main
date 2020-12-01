1c7b6578c09bc47f7d0ef8c92423bf0987683e7f
Majbendix-1825 <75244704+Majbendix-1825@users.noreply.github.com>
2020-12-01 19:00:11 +0100

    Sat links på genre på knapperne

--- myBooking/concerts.py
+++ myBooking/concerts.py
@@ -4,9 +4,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/pop") # Here we define which subpage the following code relates to
 def pop(): #Define pop site#
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts") #Execute the concerts database
-    return render_template("P7.html", rows=rows) #Return pop#
+    rows = db_cursor.execute("SELECT * FROM concerts where genre='Pop'") #Find the concerts matching the criteria, e.g. genre, date, etc.
+    return render_template("P7.html", rows=rows, Overskrift="Pop concerts") #Return pop#
+
+@app.route("/jazz")
+def Jazz(): #Define pop site#
+    db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
+    db_cursor = db.cursor() #Connect database to HTML
+    rows = db_cursor.execute("SELECT * FROM concerts where genre='Jazz'") #Execute the concerts database
+    return render_template("P7.html", rows=rows) #Return #
+
+@app.route("/")
+def Frontpage(): #Define pop site#
+    return render_template("Parent.html") #Return #
+
+
