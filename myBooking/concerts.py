1bab3f0dd8774a57f3feba02d2409b2b5b373ba7
ililtorp <60934620+ililtorp@users.noreply.github.com>
2020-12-01 13:50:34 +0100

    Add files via upload

---
+++ myBooking/concerts.py
@@ -0,0 +1,11 @@
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def pop(): #Define pop site#
+    db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
+    db_cursor = db.cursor() #Connect database to HTML
+    rows = db_cursor.execute("SELECT * FROM concerts") #Execute the concerts database
+    return render_template("P7.html", rows=rows) #Return pop#
