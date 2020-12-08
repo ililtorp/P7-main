f5784f59fb825b33173e4a1185bb2c7c264de9d3
ililtorp <60934620+ililtorp@users.noreply.github.com>
2020-12-08 11:01:06 +0100

    Opdateret kode

--- myBooking/concerts.py
+++ myBooking/concerts.py
@@ -9,46 +9,53 @@ app = Flask(__name__) #Create Flask app
 def Frontpage(): #Define pop site#
     return render_template("Parent.html") #Return
 
-@app.route("/pop/<selected_date>", methods=["GET"]) # Here we define which subpage the following code relates to
-def pop(selected_date): #Define pop site
+@app.route("/pop") # Here we define which subpage the following code relates to
+def pop(): #Define pop site#
     title = "Pop concerts"
-    if request.method == "GET":
-        selected_date = datetime.strptime(selected_date, "%D-%m-%y").date()
-    date = request.args.get('date') #Save the date in URL
+    date = request.args.get('date') #Gets the date from the URL
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
-    db_cursor = db.cursor() #Connect database to HTML
+    db_cursor = db.cursor() #Connect to database 
     rows = db_cursor.execute("SELECT * FROM concerts where genre='Pop' and Dato=?", (date,)) #Find the concerts matching the criteria genre, date, etc.
-    return render_template("P7.html", rows=rows, title=title) #Return pop - her skal jeg undersøge hvordan man gør overskriften dynamisk
-
+    return render_template("P7.html", rows=rows, title=title) #Return pop - her skal jeg undersøge hvordan man gør overskriften dynamisk 
+    
 
 @app.route("/jazz")
-def Jazz(): #Define jazz site
+def Jazz(): #Define jazz site#
     title = "Jazz concerts"
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect database to HTML
     rows = db_cursor.execute("SELECT * FROM concerts where genre='Jazz'") #Execute the concerts database
-    return render_template("P7.html", rows=rows, title=title) #Return
+    return render_template("P7.html", rows=rows, title=title) #Return #
 
 @app.route("/classic")
-def Klassisk(): #Define classic site
+def Klassisk(): #Define classic site#
     title = "Classic concerts"
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect database to HTML
     rows = db_cursor.execute("SELECT * FROM concerts where genre='Klassisk'") #Execute the concerts database
-    return render_template("P7.html", rows=rows, title=title) #Return
+    return render_template("P7.html", rows=rows, title=title) #Return #
 
 @app.route("/rock")
-def Rock(): #Define rock site
+def Rock(): #Define rock site#
     title = "Rock concerts"
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect database to HTML
     rows = db_cursor.execute("SELECT * FROM concerts where genre='Rock'") #Execute the concerts database
-    return render_template("P7.html", rows=rows, title=title) #Return
+    return render_template("P7.html", rows=rows, title=title) #Return #
 
 @app.route("/hiphop")
-def Hiphop(): #Define hiphop site
+def Hiphop(): #Define hiphop site#
     title = "Hiphop concerts"
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect database to HTML
     rows = db_cursor.execute("SELECT * FROM concerts where genre='Hiphop'") #Execute the concerts database
-    return render_template("P7.html", rows=rows, title=title) #Return
+    return render_template("P7.html", rows=rows, title=title) #Return #
+
+@app.route("/chosenConcert")
+def chosenConcert(): 
+    concertID = request.args.get('concertID') #Gets the date from the URL
+    db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
+    db_cursor = db.cursor() #Connect to database 
+    rows = db_cursor.execute("SELECT * FROM concerts where Koncert_Id=?", (concertID,)) #Find the concerts matching the criteria genre, date, etc.
+    return render_template("Concert.html", rows=rows) #Return pop - her skal jeg undersøge hvordan man gør overskriften dynamisk 
+    
