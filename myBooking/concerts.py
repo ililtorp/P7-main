1857fa81a13d8fe3bb4d28313ba7b8cbf9a5c90e
lassedahl2 <72791070+lassedahl2@users.noreply.github.com>
2020-12-09 09:09:10 +0100

    sa

--- myBooking/concerts.py
+++ myBooking/concerts.py
@@ -5,50 +5,6 @@ from datetime import datetime
 
 app = Flask(__name__) #Create Flask app
 
-@app.route("/") #Frontpage
-def Frontpage(): #Define pop site#
-    return render_template("Parent.html") #Return
-
-@app.route("/pop/<selected_date>", methods=["GET"]) # Here we define which subpage the following code relates to
-def pop(selected_date): #Define pop site
-    title = "Pop concerts"
-    if request.method == "GET":
-        selected_date = datetime.strptime(selected_date, "%D-%m-%y").date()
-    date = request.args.get('date') #Save the date in URL
-    db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
-    db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts where genre='Pop' and Dato=?", (date,)) #Find the concerts matching the criteria genre, date, etc.
-    return render_template("P7.html", rows=rows, title=title) #Return pop - her skal jeg undersøge hvordan man gør overskriften dynamisk
-
-
-@app.route("/jazz")
-def Jazz(): #Define jazz site
-    title = "Jazz concerts"
-    db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
-    db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts where genre='Jazz'") #Execute the concerts database
-    return render_template("P7.html", rows=rows, title=title) #Return
-
-@app.route("/classic")
-def Klassisk(): #Define classic site
-    title = "Classic concerts"
-    db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
-    db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts where genre='Klassisk'") #Execute the concerts database
-    return render_template("P7.html", rows=rows, title=title) #Return
-
-@app.route("/rock")
-def Rock(): #Define rock site
-    title = "Rock concerts"
-    db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
-    db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts where genre='Rock'") #Execute the concerts database
-    return render_template("P7.html", rows=rows, title=title) #Return
-
-@app.route("/hiphop")
-def Hiphop(): #Define hiphop site
-    title = "Hiphop concerts"
-    db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
-    db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts where genre='Hiphop'") #Execute the concerts database
-    return render_template("P7.html", rows=rows, title=title) #Return
+@app.route("/mytickets")
+def mytickets():
+    return render_template("mytickets.html")
