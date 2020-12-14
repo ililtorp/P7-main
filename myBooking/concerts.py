6d59b42880a5789a4ac58e2b0d75fed00693ba76
Majbendix-1825 <75244704+Majbendix-1825@users.noreply.github.com>
2020-12-14 09:14:36 +0100

    CSS og billeder virker nu

--- myBooking/concerts.py
+++ myBooking/concerts.py
@@ -10,13 +10,13 @@ def Frontpage(): #Define pop site#
     return render_template("Parent.html") #Return
 
 @app.route("/pop") # Here we define which subpage the following code relates to
-def pop(): #Define pop site#
+def pop(): 
     title = "Pop concerts"
     date = request.args.get('date') #Gets the date from the URL
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect to database 
-    rows = db_cursor.execute("SELECT * FROM concerts where Genre1='Pop'and Dato=?", (date,)) #Find the concerts matching the criteria genre, date, etc.
-    return render_template("P7.html", rows=rows, title=title) #Return pop - her skal jeg undersøge hvordan man gør overskriften dynamisk 
+    rows = db_cursor.execute("SELECT * FROM concerts where Dato=? and Genre1='Pop' or Genre2='Pop'", (date,)) #Find the concerts matching the criteria 
+    return render_template("P7.html", rows=rows, title=title) 
     
 
 @app.route("/jazz")
@@ -25,7 +25,7 @@ def Jazz(): #Define jazz site#
     date = request.args.get('date') #Gets the date from the URL
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts where Genre1='Jazz'and Dato=?", (date,)) #Execute the concerts database
+    rows = db_cursor.execute("SELECT * FROM concerts where Dato=? and Genre1='Jazz' or Genre2='Jazz'", (date,)) #Execute the concerts database
     return render_template("P7.html", rows=rows, title=title) #Return #
 
 @app.route("/classic")
@@ -34,7 +34,7 @@ def Klassisk(): #Define classic site#
     date = request.args.get('date') #Gets the date from the URL
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts where Genre1='Klassisk'and Dato=?", (date,)) #Execute the concerts database
+    rows = db_cursor.execute("SELECT * FROM concerts where Dato=? and Genre1='Klassisk' or Genre2='Klassisk'", (date,)) #Execute the concerts database
     return render_template("P7.html", rows=rows, title=title) #Return #
 
 @app.route("/rock")
@@ -43,7 +43,7 @@ def Rock(): #Define rock site#
     date = request.args.get('date') #Gets the date from the URL
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts where Genre1='Rock'and Dato=?", (date,)) #Execute the concerts database
+    rows = db_cursor.execute("SELECT * FROM concerts where Dato=? and Genre1='Rock' or Genre2='Rock'", (date,)) #Execute the concerts database
     return render_template("P7.html", rows=rows, title=title) #Return #
 
 @app.route("/hiphop")
@@ -52,16 +52,25 @@ def Hiphop(): #Define hiphop site#
     date = request.args.get('date') #Gets the date from the URL
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect database to HTML
-    rows = db_cursor.execute("SELECT * FROM concerts where Genre1='Hiphop'and Dato=?", (date,)) #Execute the concerts database
+    rows = db_cursor.execute("SELECT * FROM concerts where Dato=? and Genre1='Hiphop' or Genre2='Hiphop'", (date,))#Execute the concerts database
+    return render_template("P7.html", rows=rows, title=title) #Return #
+
+@app.route("/metal")
+def metal(): #Define hiphop site#
+    title = "Metal concerts"
+    date = request.args.get('date') #Gets the date from the URL
+    db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
+    db_cursor = db.cursor() #Connect database to HTML
+    rows = db_cursor.execute("SELECT * FROM concerts where Dato=? and Genre1='Metal' or Genre2='Metal'", (date,))#Execute the concerts database
     return render_template("P7.html", rows=rows, title=title) #Return #
 
 @app.route("/chosenConcert")
 def chosenConcert(): 
-    concertID = request.args.get('concertID') #Gets the date from the URL
+    concertID = request.args.get('concertID') #Gets the concert id from the URL
     db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
     db_cursor = db.cursor() #Connect to database 
-    rows = db_cursor.execute("SELECT * FROM concerts where Koncert_Id=?", (concertID,)) #Find the concerts matching the criteria genre, date, etc.
-    return render_template("Concert.html", rows=rows) #Return pop - her skal jeg undersøge hvordan man gør overskriften dynamisk 
+    rows = db_cursor.execute("SELECT * FROM concerts where Koncert_Id=?", (concertID,)) #Find the concerts matching the concert ID
+    return render_template("Concert.html", rows=rows) 
 
 @app.route("/booking", methods=["GET", "POST"])
 def booking(): 
@@ -69,7 +78,7 @@ def booking():
         concertID = request.args.get('concertID') #Gets the date from the URL
         db = sqlite3.connect("concerts.db", isolation_level=None) #Connect database to HTML
         db_cursor = db.cursor() #Connect to database 
-        rows = db_cursor.execute("SELECT * FROM concerts where Koncert_Id=?", (concertID,)) #Find the concerts matching the criteria genre, date, etc.
+        rows = db_cursor.execute("SELECT * FROM concerts where Koncert_Id=?", (concertID,)) #Find the concerts matching the concert ID
         return render_template("booking.html", rows=rows) #Return booking.html - redirects to the booking page
     else:
         name = request.form.get("name")
@@ -81,7 +90,7 @@ def booking():
         db_cursor = db.cursor() #Connect to database 
         db_cursor.execute("INSERT INTO kundedata (name, email, phone, ConcertID, NTickets) VALUES (?, ?, ?, ?, ?)", (name, email, phone, ConcertID, NTickets))
         booking_ID = db_cursor.lastrowid
-        return redirect("/confirmation?bookingID="+str(booking_ID)+"&ConcertID="+str(ConcertID)) #Her skal vi lave et link, til når man succesfuldt har booket sin billet. 
+        return redirect("/confirmation?bookingID="+str(booking_ID)+"&ConcertID="+str(ConcertID)) 
 
 @app.route("/confirmation")
 def confirmation():
